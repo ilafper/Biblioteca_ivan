02814f1d1a6339d95de1f74ce814d25156a47f01
ialfper <ivanaalfaya@gmail.com>
2024-03-15 16:04:46 +0100

    Nuevas funciones(inicio sesion, registro, cola reserva)

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,7 @@ public class Main {
 
                                     switch (menu2) {
                                         case 1:
+                                            //hola
                                             System.out.print(usuario.toString());
                                             break;
                                         case 2:
